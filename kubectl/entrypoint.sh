7d9ad331dc35a3fd229d118b023cd64c34b7cc6a
Sergey Grebnov <sergei.grebnov@gmail.com>
2019-09-22 23:10:55 +0300

    Upgrade kubectl version

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -6,4 +6,15 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
+k8sversion=v1.16.0
+
+# Get the kubectl binary.
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$k8sversion/bin/linux/amd64/kubectl
+
+# Make the kubectl binary executable.
+chmod +x ./kubectl
+
+# Move the kubectl executable to /usr/local/bin.
+sudo mv ./kubectl /usr/local/bin/kubectl
+
 sh -c "kubectl $*"
