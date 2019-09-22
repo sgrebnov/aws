3f5b2f6e346dc6d7125c2ee279ec30caf2814912
Sergey Grebnov <sergei.grebnov@gmail.com>
2019-09-22 23:43:10 +0300

    Update entrypoint.sh

--- kubectl/entrypoint.sh
+++ kubectl/entrypoint.sh
@@ -3,6 +3,7 @@
 set -e
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
+echo "$KUBE_CONFIG_DATA"
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
