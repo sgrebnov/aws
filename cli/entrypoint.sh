5ac4756e2253df3fa8847eee116990fd38f0fdf3
Sergey Grebnov <sergei.grebnov@gmail.com>
2019-09-23 01:58:45 +0300

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -10,9 +10,6 @@ set -e
 
 # Capture output
 #output=$( sh -c "aws $*" )
-#aws --profile default configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-#aws --profile default configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-#aws --profile default configure set region "$AWS_DEFAULT_REGION"
 
 # Preserve output for consumption by downstream actions
 #echo "$output" > "${HOME}/${GITHUB_ACTION}.${AWS_DEFAULT_OUTPUT}"
@@ -27,8 +24,6 @@ k8sversion=v1.16.0
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$k8sversion/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 
-#output=$( sh -c "aws eks --region '$AWS_DEFAULT_REGION' update-kubeconfig --name projects" )
-output=$( sh -c "aws sts get-caller-identity --debug" )
-echo "$output"
+aws eks --region '$AWS_DEFAULT_REGION' update-kubeconfig --name projects
                       
 sh -c "$*"
