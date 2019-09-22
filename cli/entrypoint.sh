eb751da8185a799a7accd686fe0aa7ce5e2eae0b
Sergey Grebnov <sergei.grebnov@gmail.com>
2019-09-23 01:53:26 +0300

    Update entrypoint.sh

--- cli/entrypoint.sh
+++ cli/entrypoint.sh
@@ -3,16 +3,16 @@
 set -e
 
 # Respect AWS_DEFAULT_REGION if specified
-[ -n "$AWS_DEFAULT_REGION" ] || export AWS_DEFAULT_REGION=us-east-1
+[ -n "$AWS_DEFAULT_REGION" ] || export AWS_DEFAULT_REGION=us-east-2
 
 # Respect AWS_DEFAULT_OUTPUT if specified
 [ -n "$AWS_DEFAULT_OUTPUT" ] || export AWS_DEFAULT_OUTPUT=json
 
 # Capture output
 #output=$( sh -c "aws $*" )
-aws --profile default configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
-aws --profile default configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
-aws --profile default configure set region "$AWS_DEFAULT_REGION"
+#aws --profile default configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+#aws --profile default configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+#aws --profile default configure set region "$AWS_DEFAULT_REGION"
 
 # Preserve output for consumption by downstream actions
 #echo "$output" > "${HOME}/${GITHUB_ACTION}.${AWS_DEFAULT_OUTPUT}"
